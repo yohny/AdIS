e608d70400ce0ebd31bc728c8eef1c609b47720e
Ján Neščivera <yohny@users.noreply.github.com>
2012-07-19 20:42:15 +0000

    refactored userSection

--- app/templates/partials/userSection.php
+++ app/templates/partials/userSection.php
@@ -13,40 +13,36 @@
  */
 
 if (Context::getInstance()->getUser()): ?>
-    <fieldset>
-        <legend>Správa</legend>
-        <a href="/profil">Profil</a>
-    <?php
-    if (Context::getInstance()->getUser()->kategoria == User::ROLE_INZER)
-        echo "
-            <a href=\"/bannery\">Bannery</a>
-            <a href=\"/statistika\">Štatistika</a>";
-    elseif (Context::getInstance()->getUser()->kategoria == User::ROLE_ZOBRA)
-        echo "
-            <a href=\"/reklamy\">Reklamy</a>
-            <a href=\"/statistika\">Štatistika</a>";
-    elseif (Context::getInstance()->getUser()->kategoria == User::ROLE_ADMIN)
-        echo "
-            <a href=\"/statistika_admin\">Štatistika</a>
-            <a href=\"javascript: void(0);\"><span class=\"r\">ADMIN</span></a>";
-    ?>
+<fieldset>
+    <legend>Správa</legend>
+    <a href="/profil">Profil</a>
+<?php if (Context::getInstance()->getUser()->kategoria == User::ROLE_INZER): ?>
+    <a href="/bannery">Bannery</a>
+    <a href="/statistika">Štatistika</a>
+<?php elseif (Context::getInstance()->getUser()->kategoria == User::ROLE_ZOBRA): ?>
+    <a href="/reklamy">Reklamy</a>
+    <a href="/statistika">Štatistika</a>
+<?php elseif (Context::getInstance()->getUser()->kategoria == User::ROLE_ADMIN): ?>
+    <a href="/statistika_admin">Štatistika</a>
+    <a href="#"><span class="r">ADMIN</span></a>
+<?php endif; ?>
 </fieldset>
 <h3>používateľ</h3>
 <span style="font-variant: small-caps;font-weight: bolder;"><?php echo $_SESSION['user']; ?></span>
 <form style="display: inline-block" method="post" action="/action/odhlas">
-    <a style="margin-left:10px;" href="#" onclick="this.parentNode.submit();return false;">(Odlásiť)</a>
+    <a style="margin-left:10px;" href="#" onclick="this.parentNode.submit();return false;">(Odhlásiť)</a>
     <input type="hidden" name="action" value="logout" />
     <input type="hidden" name="csrf_token" value="<?php echo Context::getInstance()->getCsrfToken(); ?>" />
 </form>
 <?php else: ?>
-        <h3>používateľ</h3>
-        <form name="log_form" action="/action/prihlas" method="post">
-            <table>
-                <tr><td>Login<br/><input type="text" name="login" maxlength="10" /></td></tr>
-                <tr><td>Heslo<br/><input type="password" name="heslo" maxlength="10" /></td></tr>
-                <tr><td><input type="button" value="Prihlásiť" onclick="spracuj_log()" /><a style="margin-left:10px;" href="/registracia">Registruj</a></td></tr>
-                <tr><td><div id="log_errbox" class="errbox"></div></td></tr>
-            </table>
-            <input type="hidden" name="csrf_token" value="<?php echo Context::getInstance()->getCsrfToken(); ?>" />
-        </form>
+<h3>používateľ</h3>
+<form name="log_form" action="/action/prihlas" method="post">
+    <table>
+        <tr><td>Login<br/><input type="text" name="login" maxlength="10" /></td></tr>
+        <tr><td>Heslo<br/><input type="password" name="heslo" maxlength="10" /></td></tr>
+        <tr><td><input type="button" value="Prihlásiť" onclick="spracuj_log()" /><a style="margin-left:10px;" href="/registracia">Registruj</a></td></tr>
+        <tr><td><div id="log_errbox" class="errbox"></div></td></tr>
+    </table>
+    <input type="hidden" name="csrf_token" value="<?php echo Context::getInstance()->getCsrfToken(); ?>" />
+</form>
 <?php endif; ?>

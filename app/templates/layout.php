6cbf63eee426ad1f0bd7bbbcedcb385fc06c717b
Ján Neščivera <yohny@users.noreply.github.com>
2012-11-18 13:12:25 +0000

    layout improvements - removed #container div  from layout (modified CSS accordingly), added copyright notice to signature

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -19,92 +19,90 @@
         <?php foreach (Context::getInstance()->getResponse()->getResources() as $resource) echo $resource . "\n"; ?>
     </head>
     <body>
-        <div id="container">
+        <div class="top_left corner"></div>
+        <div class="top_right corner"></div>
+        <div class="bottom_left corner"></div>
+        <div class="bottom_right corner"></div>
+
+        <div id="top">
             <div class="top_left corner"></div>
             <div class="top_right corner"></div>
             <div class="bottom_left corner"></div>
             <div class="bottom_right corner"></div>
+            <h1 class="above">Ad-IS</h1>
+            <h1 class="below">Ad-IS</h1>
+            <h2>Reklamný systém</h2>
+        </div>
 
-            <div id="top">
+        <div id="left">
+            <div id="menu">
                 <div class="top_left corner"></div>
                 <div class="top_right corner"></div>
                 <div class="bottom_left corner"></div>
                 <div class="bottom_right corner"></div>
-                <h1 class="above">Ad-IS</h1>
-                <h1 class="below">Ad-IS</h1>
-                <h2>Reklamný systém</h2>
+                <h3>menu</h3>
+
+                <fieldset>
+                    <legend>Ad-IS</legend>
+                    <a href="/about">O systéme</a>
+                    <a href="/faq">FAQ</a>
+                </fieldset>
+
+                <?php require_once TEMPLATES_DIR.'/partials/userSection.php'; ?>
             </div>
 
-            <div id="left">
-                <div id="menu">
-                    <div class="top_left corner"></div>
-                    <div class="top_right corner"></div>
-                    <div class="bottom_left corner"></div>
-                    <div class="bottom_right corner"></div>
-                    <h3>menu</h3>
-
-                    <fieldset>
-                        <legend>Ad-IS</legend>
-                        <a href="/about">O systéme</a>
-                        <a href="/faq">FAQ</a>
-                    </fieldset>
-
-                    <?php require_once TEMPLATES_DIR.'/partials/userSection.php'; ?>
-                </div>
-
-                <div id="nove">
-                    <div class="top_left corner"></div>
-                    <div class="top_right corner"></div>
-                    <div class="bottom_left corner"></div>
-                    <div class="bottom_right corner"></div>
-                    <h3>info</h3>
-                    <p class="g">bežný oznam</p>
-                    <p class="r">dôležitý oznam</p>
-                    <p>
-                        <a href="http://validator.w3.org/check?uri=referer">
-                            <img src="/img/valid-xhtml10.png" alt="val_xhtml" />
-                        </a>
-                    </p>
-                    <p>
-                        <a href="http://jigsaw.w3.org/css-validator/check/referer">
-                            <img src="/img/vcss.gif" alt="val_css" />
-                        </a>
-                    </p>
-                    <p>
-                        schéma:
-                        <img id="img1" src="/img/sch1.png" onclick="set_scheme(1)" alt="s1" />
-                        <img id="img2" src="/img/sch2.png" onclick="set_scheme(2)" alt="s2" />
-                        <img id="img3" src="/img/sch3.png" onclick="set_scheme(3)" alt="s3" />
-                    </p>
-                    <script language="javascript" type="text/javascript">set_scheme(schema);</script>
-                </div>
-                <a href="mailto:jan.nescivera@gmail.com" id="ja" title="admin&amp;webmaster">&lt;Yohny&gt;</a>
-            </div><!-- end div#left --><div id="main">
+            <div id="nove">
                 <div class="top_left corner"></div>
                 <div class="top_right corner"></div>
                 <div class="bottom_left corner"></div>
                 <div class="bottom_right corner"></div>
-                <h3><?php echo Context::getInstance()->getResponse()->getHeading(); ?></h3>
-                <?php if($flash=Context::getInstance()->getResponse()->getFlash()): ?>
-                <div class="flash">
-                    <div class="top_left corner"></div>
-                    <div class="top_right corner"></div>
-                    <div class="bottom_left corner"></div>
-                    <div class="bottom_right corner"></div>
-                    <?php echo $flash; ?>
-                </div>
-                <?php endif; ?>
-                <?php if(Context::getInstance()->getResponse()->error){ ?>
-                <div class="error">
-                    <div class="top_left corner"></div>
-                    <div class="top_right corner"></div>
-                    <div class="bottom_left corner"></div>
-                    <div class="bottom_right corner"></div>
-                    <?php echo Context::getInstance()->getResponse(); ?>
-                </div>
-                <?php } else echo Context::getInstance()->getResponse(); ?>
-                <hr/>
-            </div><!-- end div#main -->
-        </div><!-- end div#container -->
+                <h3>info</h3>
+                <p class="g">bežný oznam</p>
+                <p class="r">dôležitý oznam</p>
+                <p>
+                    <a href="http://validator.w3.org/check?uri=referer">
+                        <img src="/img/valid-xhtml10.png" alt="val_xhtml" />
+                    </a>
+                </p>
+                <p>
+                    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                        <img src="/img/vcss.gif" alt="val_css" />
+                    </a>
+                </p>
+                <p>
+                    schéma:
+                    <img id="img1" src="/img/sch1.png" onclick="set_scheme(1)" alt="s1" />
+                    <img id="img2" src="/img/sch2.png" onclick="set_scheme(2)" alt="s2" />
+                    <img id="img3" src="/img/sch3.png" onclick="set_scheme(3)" alt="s3" />
+                </p>
+                <script language="javascript" type="text/javascript">set_scheme(schema);</script>
+            </div>
+            <a href="mailto:jan.nescivera@gmail.com" id="ja" title="admin&amp;webmaster">&lt;Yohny&gt; &copy; <?php echo date('Y'); ?></a>
+        </div><!-- end div#left --><div id="main">
+            <div class="top_left corner"></div>
+            <div class="top_right corner"></div>
+            <div class="bottom_left corner"></div>
+            <div class="bottom_right corner"></div>
+            <h3><?php echo Context::getInstance()->getResponse()->getHeading(); ?></h3>
+            <?php if($flash=Context::getInstance()->getResponse()->getFlash()): ?>
+            <div class="flash">
+                <div class="top_left corner"></div>
+                <div class="top_right corner"></div>
+                <div class="bottom_left corner"></div>
+                <div class="bottom_right corner"></div>
+                <?php echo $flash; ?>
+            </div>
+            <?php endif; ?>
+            <?php if(Context::getInstance()->getResponse()->error){ ?>
+            <div class="error">
+                <div class="top_left corner"></div>
+                <div class="top_right corner"></div>
+                <div class="bottom_left corner"></div>
+                <div class="bottom_right corner"></div>
+                <?php echo Context::getInstance()->getResponse(); ?>
+            </div>
+            <?php } else echo Context::getInstance()->getResponse(); ?>
+            <hr/>
+        </div><!-- end div#main -->
     </body>
 </html>
